0540477c8374e6d6526ceb8573a3a6ac6ab2bbf6
shaliginandrew <shaligin.andrew@gmail.com>
2021-12-07 17:52:07 +0300

    Добавление отображения всех задач без фильтрации по категории

---
+++ src/app/views/tasks/tasks.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {Task} from "../../model/Task";
+import {DataHandlerService} from "../../service/data-handler.service";
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private DataHandler: DataHandlerService) {
+  }
+
+  ngOnInit(): void {
+    this.tasks = this.DataHandler.getTasks();
+  }
+
+}
