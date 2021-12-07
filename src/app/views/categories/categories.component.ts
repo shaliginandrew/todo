97bdf841fc0a0be25b71bd6f4502d115bb1e0569
shaliginandrew <shaligin.andrew@gmail.com>
2021-12-07 17:08:23 +0300

    Добавление отображения категорий в левой панели

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private dataHandler: DataHandlerService) {
+  }
 
   ngOnInit(): void {
+    this.categories = this.dataHandler.getCategories();
+    console.log(this.categories);
   }
 
 }
